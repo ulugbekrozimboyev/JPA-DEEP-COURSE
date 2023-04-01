954bd3190c589fe46a7059d8e1e9539e7060a84a
Ulug'bek <ulugbekrozimboyev@gmail.com>
2023-04-01 15:32:06 +0500

    Book Shop :: deleted unused classed

--- 2.BookShopProject/src/main/java/uz/ulugbek/bookshopproject/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.ulugbek.bookshopproject.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import uz.ulugbek.bookshopproject.domain.Book;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
--- 2.BookShopProject/src/main/java/uz/ulugbek/bookshopproject/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.ulugbek.bookshopproject.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import uz.ulugbek.bookshopproject.domain.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
